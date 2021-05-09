b40fad7ab50ae1389c0beb1f4e5016f83a4ae846
motua16 <shubhamgupta16novv@gmail.com>
2021-05-09 21:57:32 +0530

    added comments

--- actions/actions_2.py
+++ actions/actions_2.py
@@ -1,3 +1,4 @@
+# importing libraries
 import logging
 from typing import Any, Text, Dict, List, Union, Optional
 from rasa_sdk import Action, Tracker
@@ -9,20 +10,32 @@ logger = logging.getLogger(__name__)
 
 class ActionEntity(Action):
 
+    '''
+        this class performs the action of
+        remembering name of the user if
+        there was any name input before
+    '''
+
     def name(self) -> Text:
+
+        '''defining name of the action'''
         return "action_get_entity"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        '''
+            function to perform the task
+        '''
         slot_name = tracker.get_slot("name")
 
-        print("slotname", slot_name)
+        # if there is no name stored
         if slot_name is None:
             dispatcher.utter_message(
             text="Hi! What is your name?")
 
-        
+        # if there is a name stored
         else:
             dispatcher.utter_message(
             text="Hi " + slot_name.title() +  "! What can I do for you ?")
--- actions/actions_3.py
+++ actions/actions_3.py
@@ -1,3 +1,5 @@
+
+# importing libraries
 import logging
 from typing import Any, Text, Dict, List, Union, Optional
 from rasa_sdk import Action, Tracker
@@ -9,28 +11,44 @@ logger = logging.getLogger(__name__)
 
 class ActionEntity(Action):
 
+    '''
+        this class handles the cases if 
+        user wants previously calculated value
+        or he wants nationwide results or
+        to ask for which location he wants 
+        covid-19 stats
+    '''
+
     def name(self) -> Text:
+
+        '''
+            defined name of action
+        '''
         return "action_know_whether_previous_calculated"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        '''
+            functions to manage cases
+        '''
         slot_state = tracker.get_slot("state")
         slot_pincode = tracker.get_slot("pincode")
-        print("latest message intent ",tracker.latest_message['intent'].get('name'))
-        # print("slotname", slot_name)
+    
+        # if the user answers no to the question if he wants previous result then he will get nationwode results
         if  tracker.latest_message['intent'].get('name')=='deny':
             dispatcher.utter_message(
             text="Showing Nationwide results")
 
-
+        # first time covid stats check
         elif (slot_state is None and slot_pincode is None) :
             dispatcher.utter_message(
-            text="Which state/city/pincode do you want to know about?")
+            text="Which pincode/state/city do you want to know about?")
 
-        
+        # recently shown results
         else:
             dispatcher.utter_message(
-            text=f"Do you want recently shown results?")
+            text=f"Do you want recently shown results of {slot_pincode}?")
 
         return []
